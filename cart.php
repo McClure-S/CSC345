ec9ff2ae965dec827af9909b9a90e395117c9daf
McClure-S <McClure-S@users.noreply.github.com>
2018-11-16 14:21:35 -0500

    Add files via upload
    Overhaul of cart system. Now feeds into "orders" table on database. creates new row upon login or order submission.
    
    Still can't add/remove items from cart.
    Does not update "OrderItems" table on database with items in cart. Simply logs the UserID and Address of the user upon submission.

--- cart.php
+++ cart.php
@@ -8,22 +8,25 @@ require_once "classes/form.php";
 require_once "classes/checkout.php";
 
 $db = new Database();
-$checkout = new Checkout($db);
 $session = new Session($db);
+$checkout = new Checkout($db);
 
 $loggedInID = $session->loggedIn();
 
 $page = new Page("Maple Blaze");
-$page->setSubtitle("Weed Eh?");
+$page->setSubtitle("Weed Ey?");
 
+//	Commented out until login thing is fixed... // 
 
-if ($loggedInID) {
+if ($loggedInID) 
+{
 	$page->addNavigationItem("Home", './');
+	$page->addNavigationItem("Manage Users", 'user.php');
+	$page->addNavigationItem("Cart",'cart.php');
 	$page->addNavigationItem("Log Out", "?logout");
-} else {
+} else 
+{
 	$page->addNavigationItem("Log In", '#" data-toggle="modal" data-target="#loginModal');
-	$page->addNavigationItem("Sign Up", '#" data-toggle="modal" data-target="#signUpModal');
-
 	//https://www.w3schools.com/bootstrap/bootstrap_modal.asp
 	$page->addContent('
 	<div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-hidden="true" style="z-index:10000">
@@ -41,24 +44,6 @@ if ($loggedInID) {
 			</div>
 		</div>
 	</div>');
-
-	$page->addContent('
-	<div class="modal fade" id="signUpModal" tabindex="-1" role="dialog" aria-hidden="true" style="z-index:10000">
-		<div class="modal-dialog" role="document">
-			<div class="modal-content">
-				<div class="modal-header">
-					<h2 class="modal-title" id="exampleModalLabel">Sign In</h2>
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-						<span aria-hidden="true">&times;</span>
-					</button>
-				</div>
-				<div class="modal-body">
-					'.$session->signUpForm().'
-
-				</div>
-			</div>
-		</div>
-	</div>');
 	
 	$session->addMessage('warning', 'You have to log in before you can use this page.');
 	$page->containerStart("edit2");
@@ -72,26 +57,29 @@ $user = new User($db, $session, $loggedInID);
 
 
 
-if (isset($_POST['OriginalID'])){
+if (isset($_POST['OriginalID']))
+{
 	User::saveForm($db,$session);
 }
 
-if(isset($_POST['submit']))
+if (isset($_POST['Address']))
 {
-	removeItem($_POST['id'], $_POST['name']);
+	Checkout::saveCheckout($db,$session);
+	Checkout::cartRefresh($db,$session,$user);
 }
-
 	$page->addContent('<div id="cardbg">');
 	$page->containerStart("edit-form");
-	//$user = new User($db, $session, "fowler1na@alma.edu", "letmein");
+	$user = new User($db, $session, "fowler1na@alma.edu", "letmein");
 	$page->addContent($user->cartCard($db, $session));
-	$page->addContent('<div id="checkoutForm">');
 	$page->addContent($checkout->checkoutForm());
-	$page->addContent('</div>');
-
 	$page->containerEnd();
 	$page->addContent('</div>');
  
+//  else {
+// 	$page->containerStart("user-table");
+// 	$page->addContent(User::allUsersTable($db, true));
+// 	$page->containerEnd();
+// }
 
 
 $page->renderPage($session);
--- checkout.php
+++ checkout.php
@@ -2,28 +2,61 @@
 /*
 checkout.php
 Handles the checkout methonds and forms.
+
 Created: 11/08/2018
 Author: Spencer McClure
 */
-require_once "classes/session.php";
-require_once "classes/page.php";
+
 require_once "classes/database.php";
-require_once "classes/user.php";
 require_once "classes/form.php";
-require_once "classes/checkout.php";
 
 class Checkout{
 	private $db;
 	private $form;
+
 	public function __construct($db){
 		$this->db = $db;
 	}
+
 	public function checkoutForm () {
 		$form = new Form("checkout-form");
-		$form->addTextInput('Name', true);
-		$form->addTextInput('Date of Birth', true);
 		$form->addTextInput('Address', true);
 		$form->addSubmit('Checkout');
 		return $form->getForm();
+	}
+
+	public static function saveCheckout($db, $session) {
+		
+
+		$address = $_POST['Address'];
+		$status = "submitted";
+
+
+
+		$stmt = $db->prepare('UPDATE orders SET status =:Status, Address =:Address WHERE OrderID=:OrderID');
+		$stmt->bindParam(':Address', $address);
+		$stmt->bindParam(':Status', $status);
+		$stmt->bindParam(':OrderID', $_SESSION['OrderID']);
+		$stmt->execute();
+
+		$session->addMessage("success","Your order has been submitted, thank you for your patronage.");
+
+		
+	}
+
+	public static function cartRefresh($db, $session, $user)
+	{
+		$UserID = $user->getID();
+		$stmt = $db->prepare('INSERT INTO orders (UserID, status) VALUES (:UserID, "cart")');
+		$stmt->bindParam(':UserID', $UserID);
+		$stmt->execute();
+
+		$stmt = $db->prepare('SELECT OrderID FROM orders WHERE UserID=:UserID AND status= "cart"');
+		$stmt->bindParam(':UserID', $UserID);
+		$stmt->execute();
+
+		$results = $stmt->fetch(PDO::FETCH_ASSOC);
+		$_SESSION['OrderID'] = $results['OrderID'];
+		return $_SESSION['cscUserID'];
 	}
 }
